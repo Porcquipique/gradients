7fd7794af127b76d10432d10ad995e6bbecbdd18
Hugo <aguilar.hugo17@gmail.com>
2020-12-22 16:27:12 +0100

    Add scipt avec commentaire. Pas encore testé

---
+++ script/script.js
@@ -0,0 +1,17 @@
+let i = Element.style.font-size;
+
+/*----------- Diminution taille police -----------*/ 
+/* Enleve 1 à la taille de la police*/
+function moins() {
+    i -= 1;
+    style.font-size = 'i';
+    console.log(i);
+}
+
+/*----------- Augmentation taille police -----------*/ 
+/* Ajoute 1 à la taille de la police*/
+function plus() {
+    i += 1;
+    style.font-size = 'i';
+    console.log(i);
+}
