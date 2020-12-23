2e653187386a8c580f3d0c3c9bfe2ac98a671688
Hugo <aguilar.hugo17@gmail.com>
2020-12-23 20:46:30 +0100

    add script generator + +/- marche mais que d'un + remove taille dans global.css

--- script/script.js
+++ script/script.js
@@ -1,19 +1,57 @@
-let i = window.getComputedStyle(document.querySelector('body')).fontSize;
-let result = document.getElementsByTagName("body")[0].style.fontSize;
-console.log(i);
+ /*- Ce script permet d'augmenter ou de diminuer la taille de la police de la page */
 
-/*----------- Diminution taille police -----------*/ 
-/* Enleve 1 à la taille de la police*/
-function moins() {
-    i -= 1;
-    result = i + 'em';
-    console.log(i);
+ /* ========== Initialisation des valeurs ==========*/
+
+    // Récupération de la valeur de fontSize dans le CSS
+const i = window.getComputedStyle(document.querySelector('body')).fontSize;
+    // Récupération de la valeur sans l'unité px
+let nbI = parseFloat(i);
+let result = '';
+
+const body = document.querySelector('body');
+// Def des button + et -
+const buttonPlus = document.querySelector('button:nth-of-type(1)');
+const buttonMoins = document.querySelector('button:nth-of-type(2)');
+
+const tailleP = document.querySelectorAll('p');
+
+/*===== Fonction modification taille ======*/
+function moins(tailleP) {
+    result = nbI - 1;
+    body.style.fontSize = result + 'px';
+    console.log(nbI);
+    return nbI;
 }
+
+function plus(tailleP) {
+    result = (nbI + 1) + 'px';
+    body.style.fontSize = result;
+    console.log(nbI);
+}
+
+/*===== Boutton HTML ======*/
+buttonMoins.addEventListener('click', () => {
+    tailleP.forEach((element) =>{
+        moins(element);
+    });
+    console.log(body.style.fontSize);
+});
+
+buttonPlus.addEventListener('click', () => {
+    tailleP.forEach((element) => {
+        plus(element);
+    });
+    console.log(body.style.fontSize);
+});
+
+
+
 
 /*----------- Augmentation taille police -----------*/ 
 /* Ajoute 1 à la taille de la police*/
-function plus() {
-    i += 1;
-    body.style.fontSize = i + 'em';
-    console.log(i);
-}
+/*function plus() {
+    nbI += 1;
+    result = nbI + 'px';
+    console.log(nbI);
+    console.log(result);
+}*/
