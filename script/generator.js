c23d6f5b7f6e5cfcbd2cc634978b9ca116fcb09d
Hugo <aguilar.hugo17@gmail.com>
2020-12-24 17:29:02 +0100

    add generator.js + commencé a faire le script + commencé le formulaire

---
+++ script/generator.js
@@ -0,0 +1,12 @@
+/****************************************
+ *       Générateur CSS - Gradiant      *
+ ****************************************/
+
+/* Init des couleurs */
+const gColor1 = document.querySelector('section:nth-of-type(2)>form>input:nth-of-type(1)').value;
+const gColor2 = document.querySelector('section:nth-of-type(2)>form>input:nth-of-type(1)').value;
+
+console.log(gColor1);
+
+/* Forme gradiant */
+const form = document.querySelector('section:nth-of-type(2)>form>select');
